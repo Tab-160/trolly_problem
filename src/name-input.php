b85cddbad02d8b8782d1b63f366764d55d2251c9
Tab-160 <rab.greenup@gmail.com>
2025-11-20 20:04:39 -0800

    We got data to the server!

---
+++ src/name-input.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="../css/style.css"/>
+	
+		<title>Trolly Problem</title>
+		<link href="../img/Logo%208.svg" rel="shortcut icon"/>
+	</head>
+	<body>
+		<h1>Welcome <?php echo $_POST["name"]; ?></h1>
+	</body>
+</html>
